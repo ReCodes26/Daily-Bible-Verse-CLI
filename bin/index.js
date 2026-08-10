c8747a92d859a31968f4000b6ffd05cfcb0eb4d1
First Last <170429152+ReCodes26@users.noreply.github.com>
2026-08-10 16:11:31 -0400

    Implement daily verse retrieval functionality in CLI

--- bin/index.js
+++ bin/index.js
@@ -2,14 +2,24 @@
 
 import { Command } from "commander";
 import chalk from "chalk";
+import { getDailyVerse } from "../lib/bible-api.js";
 
 const program = new Command();
 
 program
   .version("1.0.0")
   .description("A CLI to get a daily Bible verse")
-  .action(() => {
-    console.log(chalk.blueBright("Fetching your daily Bible verse..."));
-  });
+  .action(getBibleVerse);
 
 program.parse(process.argv);
+
+function getBibleVerse() {
+  getDailyVerse().then((data) => {
+    if (data) {
+      console.log(chalk.green(`Today's Verse: ${data.text}`));
+      console.log(chalk.yellow(`Reference: ${data.reference}`));
+    } else {
+      console.error(chalk.red("Failed to fetch the daily Bible verse."));
+    }
+  });
+}
