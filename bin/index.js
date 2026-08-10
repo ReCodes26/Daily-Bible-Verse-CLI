31e94c3b162fef330797bd5095d8e68cb266613e
First Last <170429152+ReCodes26@users.noreply.github.com>
2026-08-10 16:29:23 -0400

    Added support for other translations

--- bin/index.js
+++ bin/index.js
@@ -9,14 +9,22 @@ const program = new Command();
 program
   .version("1.0.0")
   .description("A CLI to get a daily Bible verse")
-  .action(getBibleVerse);
+  .option(
+    "-t, --translation <translation>",
+    "Set the translation of the verse. Available English translations are 'web', 'kjv', 'asv', 'bbe', 'darby', 'dra'",
+    "web",
+  )
+  .action((options) => getBibleVerse(options));
 
 program.parse(process.argv);
 
-function getBibleVerse() {
-  getDailyVerse().then((data) => {
+function getBibleVerse(options) {
+  getDailyVerse(options.translation).then((data) => {
     if (data) {
-      console.log(chalk.green(`Today's Verse: ${data.text}`));
+      console.log(
+        chalk.green(`Today's Verse:
+        ${data.text}`),
+      );
       console.log(chalk.yellow(`Reference: ${data.reference}`));
     } else {
       console.error(chalk.red("Failed to fetch the daily Bible verse."));
